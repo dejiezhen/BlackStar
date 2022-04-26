dbe9c39982f5ab9a18324e0286cfdd9fe66106c3
TJ Perez <tjperez090@gmail.com>
2022-04-26 11:30:26 +0200

    pioaoioijoizaa

--- Assets/Scripts/pizzaBehavior.cs
+++ Assets/Scripts/pizzaBehavior.cs
@@ -7,6 +7,7 @@ public class pizzaBehavior : MonoBehaviour
 
     private Rigidbody rb;
     public float moveSpeed;
+    public float rotateSpeed;
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +16,7 @@ public class pizzaBehavior : MonoBehaviour
 
         Vector3 temp = rb.velocity;
         temp.z = moveSpeed;
+        
         rb.velocity = temp;
 
     }
@@ -26,6 +28,9 @@ public class pizzaBehavior : MonoBehaviour
         {
             Destroy(gameObject);
         }
+
+        transform.Rotate(Vector3.right * Time.deltaTime * rotateSpeed);
+
 
     }
 }
