f786e7ea72883752d56731709375689b6ce95d33
Dejie <dejie.zhen@gmail.com>
2022-05-03 14:06:09 +0200

    points

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,19 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    public Text score;
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float overallScore;
+    public float playerAcceleration;
+    public GameObject playerObject;
+    private PlayerController player;
 
+    private void Start()
+    {
+        score.text = "0";
+        player = playerObject.GetComponent<PlayerController>();
+    }
     // Update is called once per frame
     void Update()
     {
-        
+        playerAcceleration = player.acceleration;
+        overallScore += Time.deltaTime * (playerAcceleration % 1000);
+        score.GetComponent<UnityEngine.UI.Text>().text = overallScore.ToString();
     }
-
 }
