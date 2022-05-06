807e4831a32f9885a7082d22128b87fd06118dc2
Dejie <dejie.zhen@gmail.com>
2022-05-06 11:46:21 +0200

    merge conflict

--- Assets/Scripts/MissileController.cs
+++ Assets/Scripts/MissileController.cs
@@ -40,11 +40,6 @@ public class MissileController : MonoBehaviour
             Debug.Log("Added additional points " + addObstaclePoint);
             gameManager.addPoints(addObstaclePoint);
         }
-
-        if (col.gameObject.CompareTag("UFO"))
-        {
-            Destroy(col.gameObject);
-        }
     }
 
 
