ef66e3740637b9fa94e73449ada524f6f1e766cd
Caroline Taft <71982054+ctaft22@users.noreply.github.com>
2022-05-09 21:19:57 +0200

    leaderboard WORKS!!!!

--- Assets/Scripts/LeaderboardController.cs
+++ Assets/Scripts/LeaderboardController.cs
@@ -4,12 +4,22 @@
 //using LootLocker.Requests;
 //using UnityEngine.UI;
 
+<<<<<<< Updated upstream
 //public class LeaderboardController : MonoBehaviour
 //{
 //    public InputField MemberID, PlayerScore;
 //    public int ID;
 //    int MaxScores = 7;
 //    public Text[] Entries;
+=======
+public class LeaderboardController : MonoBehaviour
+{
+    public InputField MemberID, PlayerScore;
+    
+    public int ID;
+    int MaxScores = 6;
+    public Text[] Entries;
+>>>>>>> Stashed changes
 
 //    private void Start()
 //    {
@@ -43,6 +53,7 @@
 //        );
 //    }
 
+<<<<<<< Updated upstream
 //    public void ShowScores()
 //    {
 //        LootLockerSDKManager.GetScoreList(ID, MaxScores, (response) =>
@@ -61,6 +72,26 @@
 //                        Entries[i].text = (i + 1).ToString() + ") 0";
 //                    }
 //                }
+=======
+    public void ShowScores()
+    {
+        LootLockerSDKManager.GetScoreList(ID, MaxScores, (response) =>
+        {
+            if (response.success)
+            {
+                LootLockerLeaderboardMember[] scores = response.items;
+                for (int i = 0; i < scores.Length; i++)
+                {
+                    Entries[i].text = (scores[i].member_id + ":   " + scores[i].score);
+                }
+                if (scores.Length < MaxScores)
+                {
+                    for (int i = scores.Length; i < MaxScores; i++)
+                    {
+                        Entries[i].text = "NONE";
+                    }
+                }
+>>>>>>> Stashed changes
                 
 //            }
 //            else
